500127a173c3fc63822fb1afa61885be0068258e
Lord_Medusin <jaegerfer7avendano@gmail.com>
2023-11-21 21:05:41 -0600

    Basics in listDrugs Component

--- frontend/src/app/components/list-drugs/list-drugs.component.ts
+++ frontend/src/app/components/list-drugs/list-drugs.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Drug } from 'src/app/interfaces/drug'
 
 @Component({
   selector: 'app-list-drugs',
   templateUrl: './list-drugs.component.html',
   styleUrls: ['./list-drugs.component.css']
 })
-export class ListDrugsComponent {
+export class ListDrugsComponent implements OnInit {
+  listDrugs: Drug[] = [
+    {name: "Panadol", description: "Para dolor de cabeza", price: 4, stock: 200},
+    {name: "Mentas", description: "Dulces", price: 1, stock: 500}
+  ]
 
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
