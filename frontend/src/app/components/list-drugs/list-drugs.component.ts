16ca854c69d1e8ac1cce159666a8df6741a3a022
Lord_Medusin <jaegerfer7avendano@gmail.com>
2023-11-21 22:35:29 -0600

    configuracion basica de ruteo

--- frontend/src/app/components/list-drugs/list-drugs.component.ts
+++ frontend/src/app/components/list-drugs/list-drugs.component.ts
@@ -8,8 +8,8 @@ import { Drug } from 'src/app/interfaces/drug'
 })
 export class ListDrugsComponent implements OnInit {
   listDrugs: Drug[] = [
-    {name: "Panadol", description: "Para dolor de cabeza", price: 4, stock: 200},
-    {name: "Mentas", description: "Dulces", price: 1, stock: 500}
+    {id: 1, name: "Panadol", description: "Para dolor de cabeza", price: 4, stock: 200},
+    {id: 2, name: "Mentas", description: "Dulces", price: 1, stock: 500}
   ]
 
   constructor(){
